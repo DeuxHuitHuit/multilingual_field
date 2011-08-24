b8deb24c352c028f0102a927f0823768b925e10e
6ui11em <6ui11em1@gmail.com>
2011-08-24 19:08:20 +0200

    Fix issue #16 - text limit mow works for all languages

--- assets/multilingual_field.publish.js
+++ assets/multilingual_field.publish.js
@@ -10,31 +10,35 @@ jQuery(document).ready(function() {
 		var input = self.find('input, textarea');
 		
 		if (input.attr('length') < 1) return;
-		
+
 		var optional = self.find('i');
 		var message = optional.text();
-		
-		var update = function() {
-			var length = input.val().length;
-			var limit = input.attr('length');
-			var remaining = limit - length;
+
+		jQuery(input).each(function() {
+			var $input = jQuery(this);
 			
-			optional
-				.text(message.replace('$1', remaining).replace('$2', limit))
-				.removeClass('invalid');
+			var update = function() {
+				var length = $input.val().length;
+				var limit = $input.attr('length');
+				var remaining = limit - length;
+				
+				optional
+					.text(message.replace('$1', remaining).replace('$2', limit))
+					.removeClass('invalid');
+				
+				if (remaining < 0) {
+					optional.addClass('invalid');
+				}
+			};
 			
-			if (remaining < 0) {
-				optional.addClass('invalid');
-			}
-		};
-		
-		input.bind('blur', update);
-		input.bind('change', update);
-		input.bind('focus', update);
-		input.bind('keypress', update);
-		input.bind('keyup', update);
-		
-		update();
+			$input.bind('blur', update);
+			$input.bind('change', update);
+			$input.bind('focus', update);
+			$input.bind('keypress', update);
+			$input.bind('keyup', update);
+			
+			update();
+		});
 	});
 });
 
