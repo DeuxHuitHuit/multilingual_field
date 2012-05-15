38202e994ac0a97a28d224b601a605c8d46f2ebd
Vlad Ghita <vlad_micutul@yahoo.com>
2012-05-15 18:00:01 +0300

    Release 2.0

--- assets/multilingual_field.publish.js
+++ assets/multilingual_field.publish.js
@@ -1,104 +1,38 @@
-/*-----------------------------------------------------------------------------
-	Text Box Interface
------------------------------------------------------------------------------*/
-	
-jQuery(document).ready(function() {
-	jQuery('.field-multilingual').each(function() {
-		var self = jQuery(this);
-		var field = new MultilingualField(jQuery(this));	
+;(function($, undefined){
 
-		var input = self.find('input, textarea');
-		
-		if (input.attr('length') < 1) return;
+	$(document).ready(function() {
+		$('div.field-multilingual-textbox').each(function() {
+			var $field = $(this);
 
-		var optional = self.find('i');
-		var message = optional.text();
+			var update = function(e) {
+				if( e.data.msg.indexOf('$1') !== -1  ){
+					var length = e.data._input.val().length;
+					var limit = e.data._input.attr('length');
+					var remaining = limit - length;
 
-		jQuery(input).each(function() {
-			var $input = jQuery(this);
-			
-			var update = function() {
-				var length = $input.val().length;
-				var limit = $input.attr('length');
-				var remaining = limit - length;
-				
-				optional
-					.text(message.replace('$1', remaining).replace('$2', limit))
-					.removeClass('invalid');
-				
-				if (remaining < 0) {
-					optional.addClass('invalid');
+					e.data._opt
+						.text(e.data.msg.replace('$1', remaining).replace('$2', limit))
+						.removeClass('invalid');
+
+					if (remaining < 0) {
+						e.data._opt.addClass('invalid');
+					}
 				}
 			};
-			
-			$input.bind('blur', update);
-			$input.bind('change', update);
-			$input.bind('focus', update);
-			$input.bind('keypress', update);
-			$input.bind('keyup', update);
-			
-			update();
+
+			$field.find('i').each(function(){
+				var $_opt = $(this);
+				var lang_code = $_opt.data('lang_code');
+
+				var msg = $_opt.text();
+				var $_input = $field.find('.tab-panel.tab-'+lang_code).find('input, textarea');
+				var data = {_opt: $_opt, _input: $_input, msg: msg};
+
+				$_input.on('blur change focus keypress keyup', data, update);
+
+				update({data:data});
+			});
 		});
 	});
-});
 
-function MultilingualField(field) {
-	this.field = field;
-	
-	this.init();
-}
-
-MultilingualField.prototype.init = function() {
-	var self = this,
-		activeTab = this.field.find('ul.tabs li.active');
-
-	// Fallback to first tab if no tab is set as active by default
-	if (activeTab.length == 0) {
-		activeTab = this.field.find('ul.tabs li:eq(0)');
-	}
-
-	// bind tab events
-	this.field.find('ul.tabs li').bind('click', function(e) {
-		e.preventDefault();
-		self.setActiveTab(jQuery(this).attr('class').split(' ')[0]);
-	});
-	
-	// Show the active tab
-	this.setActiveTab(activeTab.attr('class').split(' ')[0]);
-	
-	// Set unique id for tynymce support
-	this.field.find('textarea.tinymce').each(function () {
-		this.id = this.name;
-	});
-}
-
-MultilingualField.prototype.setActiveTab = function(tab_name) {
-	var self = this;
-	
-	// hide all tab panels
-	jQuery('.field-multilingual').find('.tab-panel').hide();
-	
-	// find the desired tab and activate the tab and its panel
-	jQuery('.field-multilingual ul.tabs li').each(function() {
-		var tab = jQuery(this);
-
-		if (tab.hasClass(tab_name)) {
-			tab.addClass('active');
-			
-			var panel = tab.parent().parent().find('.tab-' + tab_name);
-			panel.show();
-
-			// Resize hidden tinyMCE to fit to the correct size 
-			if (panel.find('textarea.tinymce').length > 0) {
-				var input = panel.find('textarea.tinymce');
-				panel.find('table.mceLayout').width('100%');
-				panel.find('table.mceLayout').height(input.attr('rows')+'em');
-				panel.find('table.mceLayout iframe').height(input.attr('rows')+'em');
-			}
-		} else {
-			tab.removeClass('active');
-		}
-	});
-}
-
-/*---------------------------------------------------------------------------*/
+})(this.jQuery);
